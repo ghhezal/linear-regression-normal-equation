bb124c94f90dceb0773f1b31806b388d224120eb
amine <ghhezal@gmail.com>
2026-06-27 18:30:33 +0100

    Add sklearn comparison test on California housing data

--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn.datasets import fetch_california_housing
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
 
 class linearRegressionNormal:
 
@@ -14,9 +17,28 @@ class linearRegressionNormal:
 
         A = np.linalg.inv(Xb.T @ Xb) @ Xb.T @ y
         self.intercept_ = A[0]
-        self.coef_ = A[1:]
+        self.coef_ = A[1 :]
 
     def predict(self, X):
         X = np.array(X)
 
         return X @ self.coef_ + self.intercept_
+    
+
+reg = linearRegressionNormal()
+
+X, y = fetch_california_housing(return_X_y=True)
+
+reg.fit(X, y)
+
+y_pred = reg.predict(X)
+
+print(r2_score(y, y_pred))
+
+reg_sk = LinearRegression()
+
+reg_sk.fit(X, y)
+
+y_pred = reg_sk.predict(X)
+
+print(r2_score(y, y_pred))
